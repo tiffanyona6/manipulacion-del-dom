317e8c143ca1f0f6051e555f29c8191a87d3a23a
tiffanyona6 <163582444+tiffanyona6@users.noreply.github.com>
2024-06-05 21:56:58 +0200

    actualizado

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,13 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
-import '../scss/styles.scss';
+import "../scss/styles.scss";
+
+const campoEntrada = document.getElementById("entrada");
+const campoSalida = document.getElementById("salida");
+
+campoEntrada.addEventListener("keyup", function (event) {
+  console.log(event);
+  const valoresAceptados = /^[0-9]+$/;
+  if (!campoEntrada.value.match(valoresAceptados)) {
+    campoEntrada.value = "";
+  }
+});
